b02ff07fce21fc1581fb68699e51fb735ab926bb
Your Name <you@example.com>
2024-07-20 16:55:44 +0300

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import CryptoTracker from "./Crypto-tracker/Crypto-tracker";
+import CryptoTracker from "./Components/Crypto-tracker/Crypto-tracker";
 
 function App() {
   return (
     <div className="w-full m-0 p-0 flex flex-col justify-center items-center">
-      <CryptoTracker />
+      <CryptoTracker></CryptoTracker>
     </div>
   );
 }
